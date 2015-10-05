d70f08104f32e41d976fe54b4433aa20a200cfa9
Mark Rodriguez <mark.rodriguez.dev@gmail.com>
2015-10-05 00:23:22 -0500

    improvements

---
+++ js/site-app.js
@@ -0,0 +1,10 @@
+﻿angular.module("site-app", []).controller("site-controller", ["$scope", "$http", "$location", "$anchorScroll", function ($scope, $http, $location, $anchorScroll) {
+    $scope.scrollTo = function (loc) {
+        $location.hash(loc);
+        $anchorScroll();
+    }
+    $scope.navTo = function (loc)
+    {
+        window.location = loc;
+    }
+}]);
